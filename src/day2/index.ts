a6de167a76327a7cc0c1b0b56d5034bfab5e0fee
Harry Vince <harryavince@gmail.com>
2023-12-02 10:54:27 +0000

    day2 challenge 1

---
+++ src/day2/index.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const lines = readFileSync(join(__dirname, "input.txt"), "utf-8").split(/\n/);
+// Adding extra item to array for some reason
+lines.pop();
+
+let challenge1 = 0;
+lines.forEach((line) => {
+  const splitLine = line.split(":");
+  const gameNumber = parseInt(splitLine[0].replace("Game", "").trim());
+
+  const cubes = splitLine[1].split(/,|;/);
+
+  const cubeCount = { blue: 0, red: 0, green: 0 };
+  type cubeCountKey = keyof typeof cubeCount;
+  cubes.forEach((cube) => {
+    cube = cube.trim();
+    const cubeInfo = cube.split(" ");
+    const count = parseInt(cubeInfo[0]);
+    const colour = cubeInfo[1] as cubeCountKey;
+    if (cubeCount[colour] < count) cubeCount[colour] = count;
+  });
+
+  if (cubeCount.red < 13 && cubeCount.green < 14 && cubeCount.blue < 15)
+    challenge1 += gameNumber;
+});
+
+console.log(`Challenge 1 => ${challenge1}`);
