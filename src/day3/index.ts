1ed14c657d67ec51ca87a2ae87bba8e09c80b8f9
Harry Vince <harryavince@gmail.com>
2023-12-03 18:41:29 +0000

    refactor of challenge 1 day 3 using challenge 2 solution

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -4,22 +4,6 @@ import { join } from "path";
 const lines = readFileSync(join(__dirname, "input.txt"), "utf-8").split(/\n/);
 lines.pop();
 
-const checkForSymbol = (line: string, index: number) => {
-  const subString =
-    index === 0
-      ? line.substring(index, 2)
-      : line.substring(index - 1, index + 2);
-  let limit = 0;
-  let foundSymbol = false;
-  while (limit < 3) {
-    subString.split("").forEach((char) => {
-      if (char !== "." && isNaN(parseInt(char))) foundSymbol = true;
-      limit += 1;
-    });
-  }
-  return foundSymbol;
-};
-
 const checkForNumber = (line: string, index: number) => {
   const characters = line.split("");
   const [left, right] = [index - 1, index + 1];
@@ -61,46 +45,26 @@ const checkForNumber = (line: string, index: number) => {
 let challenge1 = 0;
 for (let i = 0; i < lines.length; i++) {
   const characters = lines[i].split("");
-  const lineInformation = {
-    surroundingSymbol: false,
-    partNumber: "",
-  };
   characters.forEach((character, index) => {
-    const parsedValue = parseInt(character);
-    if (!isNaN(parsedValue)) {
-      lineInformation.partNumber += String(parsedValue);
-      if (!lineInformation.surroundingSymbol) {
-        if (i !== 0) {
-          const previousLine = checkForSymbol(lines[i - 1], index);
-          if (previousLine) lineInformation.surroundingSymbol = true;
-        }
-        if (i < lines.length - 1) {
-          const nextLine = checkForSymbol(lines[i + 1], index);
-          if (nextLine) lineInformation.surroundingSymbol = true;
-        }
+    let surroundingNumbers: Array<string> = [];
+    if (character !== "." && isNaN(parseInt(character))) {
+      if (i !== 0) {
+        const topLine = checkForNumber(lines[i - 1], index);
+        surroundingNumbers = surroundingNumbers.concat(topLine);
       }
-    } else {
-      const partNumber = parseInt(lineInformation.partNumber);
-
-      if (!isNaN(partNumber)) {
-        if (character !== "." || lineInformation.surroundingSymbol) {
-          lineInformation.surroundingSymbol = false;
-          lineInformation.partNumber = "";
-          challenge1 += partNumber;
-        }
+      if (i < lines.length - 1) {
+        const bottomLine = checkForNumber(lines[i + 1], index);
+        surroundingNumbers = surroundingNumbers.concat(bottomLine);
       }
-
-      if (character !== ".") lineInformation.surroundingSymbol = true;
-      else {
-        lineInformation.partNumber = "";
-        lineInformation.surroundingSymbol = false;
-      }
+      surroundingNumbers = surroundingNumbers.concat(
+        checkForNumber(lines[i], index),
+      );
+    }
+    if (surroundingNumbers.length > 0) {
+      const numbers = surroundingNumbers.map(Number);
+      challenge1 += numbers.reduce((a, b) => a + b);
     }
   });
-
-  if (lineInformation.surroundingSymbol) {
-    challenge1 += parseInt(lineInformation.partNumber);
-  }
 }
 
 let challenge2 = 0;
